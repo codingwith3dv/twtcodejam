71a162b8dab1e05e18b1d38653b0254fced9a13b
codingwith3dv <deveshvdddas@gmail.com>
2022-04-06 07:53:47 +0500

    :tada: Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Nav from './components/Nav.jsx'
+
+function App() {
+
+  return (
+    <div>
+      <Nav/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+function Nav() {
+  return (
+    <div>
+      <nav className="bg-gray-800 h-14 flex items-center px-4 sm:px-6 lg:px-8">
+        <div className="text-zinc-200 text-2xl font-bold font-nav-heading">Unordinary</div>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
