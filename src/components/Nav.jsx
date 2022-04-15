0de13c81870e4471cd39af597da77ebeff7c110b
codingwith3dv <deveshvdddas@gmail.com>
2022-04-15 19:36:11 +0530

    :sparkles: Add register / sign in option

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -27,15 +27,14 @@ function Nav(props) {
   function SignInForm() {
     const [email, setEmail] = useState(null);
     const [password, setPassword] = useState(null);
-    const [withEmail, setWithEmail] = useState(false);
+    const [register, setRegister] = useState(false);
 
     const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
     const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
 
     return (
       <div>
-        <div className="mt-4 flex flex-col gap-4">
-          { withEmail &&
+        <div className="mt-4 flex flex-col gap-4 divide-y divide-gray-500 justify-between">
           <div className="flex flex-col gap-4">
             <input
               type="email"
@@ -54,15 +53,22 @@ function Nav(props) {
             <button
               className="rounded-md bg-accent px-4 py-2 font-bold text-zinc-300"
               onClick={() => {
-                createUserWithEmailAndPassword(email, password);
+                if(register)
+                  createUserWithEmailAndPassword(email, password);
                 signInWithEmailAndPassword(email, password);
               }}
               >
-              Sign In
+              { !register ? "Register" : "Sign In" }
             </button>
-          </div> }
+            <p className="font-light text-zinc-300 text-xs self-center">
+              { register ? "Don't have an account?" : "Already have an account?" }
+              <span
+                className="text-violet-300"
+                onClick={() => setRegister(!register)}> { register ? "Register" : "Sign In" }</span>
+            </p>
+          </div>
         
-          <div className="flex flex-row gap-4">
+          <div className="flex flex-col gap-4 pt-4">
             <button
               className="
                 hover:bg-accent fill-violet-300
@@ -100,26 +106,6 @@ function Nav(props) {
               <div className="relative p-2 w-10 h-10">
                 <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 640 640">
                   <path d="M319.988 7.973C143.293 7.973 0 151.242 0 327.96c0 141.392 91.678 261.298 218.826 303.63 16.004 2.964 21.886-6.957 21.886-15.414 0-7.63-.319-32.835-.449-59.552-89.032 19.359-107.8-37.772-107.8-37.772-14.552-36.993-35.529-46.831-35.529-46.831-29.032-19.879 2.209-19.442 2.209-19.442 32.126 2.245 49.04 32.954 49.04 32.954 28.56 48.922 74.883 34.76 93.131 26.598 2.882-20.681 11.15-34.807 20.315-42.803-71.08-8.067-145.797-35.516-145.797-158.14 0-34.926 12.52-63.485 32.965-85.88-3.33-8.078-14.291-40.606 3.083-84.674 0 0 26.87-8.61 88.029 32.8 25.512-7.075 52.878-10.642 80.056-10.76 27.2.118 54.614 3.673 80.162 10.76 61.076-41.386 87.922-32.8 87.922-32.8 17.398 44.08 6.485 76.631 3.154 84.675 20.516 22.394 32.93 50.953 32.93 85.879 0 122.907-74.883 149.93-146.117 157.856 11.481 9.921 21.733 29.398 21.733 59.233 0 42.792-.366 77.28-.366 87.804 0 8.516 5.764 18.473 21.992 15.354 127.076-42.354 218.637-162.274 218.637-303.582 0-176.695-143.269-319.988-320-319.988l-.023.107z"/>
-                </svg>
-              </div>
-            </button>
-            <button
-              className="
-                hover:bg-accent fill-violet-300
-                hover:fill-zinc-300 hover:text-zinc-300
-                focus:bg-accent
-                focus:fill-zinc-300 focus:text-zinc-300
-                relative flex px-2 flex-row-reverse
-                justify-end items-center rounded-md border-2
-                border-accent font-bold text-violet-300
-              "
-              onClick={() => setWithEmail(!withEmail)}
-              >
-              Continue With Email
-              <div className="relative p-2 w-10 h-10">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                  <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                  <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
                 </svg>
               </div>
             </button>
