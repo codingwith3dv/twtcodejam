af9ba816830f663e3e6ae01e39e137bcb3644359
codingwith3dv <deveshvdddas@gmail.com>
2022-04-18 18:56:39 +0500

    :fire: Remove inwanted

--- src/routes/news.jsx
+++ src/routes/news.jsx
@@ -21,40 +21,6 @@ const NewsCard = (props) => {
   );
 }
 
-let testArticles = [
-  {
-    source: {
-      id: "abc-news",
-      name: "ABC News"
-    },
-    author: "Morgan Winsor",
-    title: "Russia-Ukraine live updates: At least 39 killed, 87 injured in attack on Ukrainian train station - ABC News",
-    description: "",
-    url: "https://abcnews.go.com/International/live-updates/russia-ukraine/?id=83931446",
-    urlToImage: "https://s.abcnews.com/images/International/kramatorsk-train-5-gty-rc-220408_1649415036152_hpMain_16x9_1600.jpg",
-    publishedAt: "2022-04-08T12:09:00Z",
-    content: "At least 39 people were killed in a rocket attack on a railway station in eastern Ukraine on Friday, authorities said.\r\nTwo Russian rockets struck the train station in the city of Kramatorsk in Donet… [+1998 chars]"
-  },
-  {
-    source: {
-      id: null,
-      name: "CNBC"
-    },
-    author: "Peter Schacknow",
-    title: "Stocks making the biggest moves premarket: Biogen, Spirit, Robinhood and more - CNBC",
-    description: "These are the stocks posting the largest moves before the bell.",
-    url: "https://www.cnbc.com/2022/04/08/stocks-making-the-biggest-moves-premarket-biogen-spirit-robinhood-and-more.html",
-    urlToImage: "https://image.cnbcfm.com/api/v1/image/106831762-16118534192021-01-28t165813z_294449866_rc24hl9jlwzy_rtrmadp_0_usa-stocks-open.jpeg?v=1611854525&w=1920&h=1080",
-    publishedAt: "2022-04-08T12:01:36Z",
-    content: "Check out the companies making headlines before the bell:\r\nBiogen (BIIB) Biogen fell 1% in premarket trading after the government announced that Medicare will limit coverage for Aduhelm, Biogen's Alz… [+2191 chars]",
-  },
-]
-
-for(let i = 0; i < 20; i++) {
-  testArticles.push(testArticles[0])
-  testArticles.push(testArticles[1])
-}
-
 function News() {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
